785ceef7f7eb7d25dcc586082c3c31b3527bfc12
1Jothi <rjothijesus@gmail.com>
2024-04-11 05:16:18 -0700

    jo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Weather from './Weather';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Weather App</h1>
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
+
+
